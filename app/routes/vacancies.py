3376a6e34ddce854b5424162ad3f5185ef768feb
Dalbug <pinsk.m.alibaba@gmail.com>
2025-10-11 21:31:33 +0300

    feat(crud/vacancy): add update and delete operations
    schemas/vacancy.py:
    - Added new Pydantic schemas for update and delete
    
    routes/vacancies.py:
    - Added endpoints for updating and deleting vacancies by ID
    
    crud/vacancy.py:
    - Implemented functions to update and delete records by ID

--- app/routes/vacancies.py
+++ app/routes/vacancies.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from typing import List
 from app.database import get_db
-from app.schemas.vacancy import VacancyRead, VacancyCreate
-from app.crud.vacancy import create_vacancy, get_vacancies
+from app.schemas.vacancy import VacancyRead, VacancyCreate, VacancyUpdate, VacancyDelete
+from app.crud.vacancy import create_vacancy, get_vacancies, update_vacancy, delete_vacancy
 
 router = APIRouter()
 
@@ -15,3 +15,10 @@ def list_vacancies(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)
 def add_vacancy(vacancy: VacancyCreate, db: Session = Depends(get_db)):
     return create_vacancy(db, vacancy)
 
+@router.put("/{vacancy_id}", response_model=VacancyRead)
+def up_vacancy(vacancy_id: int, vacancy: VacancyUpdate, db: Session = Depends(get_db)):
+    return update_vacancy(db, vacancy_id, vacancy)
+
+@router.delete("/{vacancy_id}", response_model=VacancyDelete)
+def del_vacancy(vacancy_id: int, db: Session = Depends(get_db)):
+    return delete_vacancy(db, vacancy_id)
