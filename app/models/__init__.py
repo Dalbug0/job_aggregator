40e47931f583f520766f92f2b83408be06f5f43e
Dalbug <pinsk.m.alibaba@gmail.com>
2025-12-27 00:25:59 +0300

    feat: add RefreshToken model and migration for database schema
    This commit introduces the RefreshToken model to manage refresh tokens in the authentication process. It updates the __init__.py file to include RefreshToken in the module exports and adds a migration script to create the refresh_tokens table in the database schema, enhancing the overall token management capabilities of the API.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .hh_token import HHToken
+from .my_api_token import RefreshToken
 from .user import User, UserAuth
 from .vacancy import Vacancy
 
-__all__ = ["HHToken", "User", "UserAuth", "Vacancy"]
+__all__ = ["HHToken", "User", "UserAuth", "Vacancy", "RefreshToken"]
